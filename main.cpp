17fc3bc85777c8a82eeab2b6754610f9e48ab538
AndonTheNguyen <andonthenguyencomputerscience@gmail.com>
2025-03-26 17:56:11 -0400

    Updated CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-using namespace std;
+int main() {
+    std::cout << "Testing SFML...\n";
 
-int main () {
-    cout << "Andon :D" << endl;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Test");
+    if (!window.isOpen()) {
+        std::cerr << "SFML window did not open.\n";
+        return 1;
+    }
+
+    sf::Clock clock;
+    while (clock.getElapsedTime().asSeconds() < 1) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    std::cout << "SFML window opened successfully!\n";
+    return 0;
 }
